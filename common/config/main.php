d77274e3b04d041ace502f0c4d940e0b90587695
harlen-mac <377304925@qq.com>
2017-05-22 23:28:36 +0800

    1.完成开发的基本配置 #2

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,14 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=wan_an',
+            'username' => 'root',
+            'password' => '123456',
+            'charset' => 'utf8',
+            'tablePrefix' => 'wa_',
+        ],
     ],
+
 ];
