fc1293d36d2a53e5bf14dc4a069751089b1c2456
刘玉鑫 <liuyuxin@angkebrand.com>
2017-06-02 17:43:36 +0800

    Squashed commit of the following:
    commit 2756428bcf5fbfc18b38983ba97847194a708757
    Merge: 9802f08 3abb01b
    Author: gaoxiaoqiang <464566602@qq.com>
    Date:   Fri Jun 2 14:49:11 2017 +0800
    
        Merge branch 'feature-issue-052' into develop
    
    commit 3abb01be1a5218b5b3c27d2d5f4342c28903a42c
    Author: gaoxiaoqiang <464566602@qq.com>
    Date:   Fri Jun 2 14:47:32 2017 +0800
    
        充值和扣除 #52

---
+++ backend/views/deposit/deposit.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\helpers\Html;
+use kartik\widgets\ActiveForm;
+use kartik\builder\Form;
+use kartik\datecontrol\DateControl;
+use kartik\widgets\FileInput;
+use kartik\widgets\SwitchInput;
+
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Deposit */
+
+$this->title = '充值';
+$this->params['breadcrumbs'][] = ['label' => '充值管理', 'url' => ['deposit']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="deposit-create">
+
+    <?php     $form = ActiveForm::begin([
+        'type' => ActiveForm::TYPE_HORIZONTAL,
+        'options' => ['enctype' => 'multipart/form-data']   // important, needed for file upload
+    ]);?>
+
+
+    <div class="row">
+        <div class="col-md-6">
+
+            <?= $form->field($model, 'member_id')->textInput(['style'=>'width:150px']) ?>
+
+            <?= $form->field($model, 'type')->dropDownList(['1'=>'金果', '2'=>'金种子'],
+                ['prompt'=>'请选择','style'=>'width:120px'])  ?>
+
+            <?= $form->field($model, 'num')->textInput(['style'=>'width:150px']) ?>
+
+            <?= $form->field($model, 'operation')->hiddenInput(['value'=>1]) ?>
+
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <?= Html::submitButton('充值', ['class' => 'btn btn-primary']) ?>
+        </div>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
