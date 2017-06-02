3abb01be1a5218b5b3c27d2d5f4342c28903a42c
gaoxiaoqiang <464566602@qq.com>
2017-06-02 14:47:32 +0800

    充值和扣除 #52

---
+++ backend/models/Deposit.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use backend\models\Member;
+
+
+/**
+ * This is the model class for table "wa_deposit".
+ *
+ * @property integer $id
+ * @property integer $member_id
+ * @property integer $type
+ * @property integer $operation
+ * @property integer $num
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class Deposit extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'wa_deposit';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['member_id', 'type', 'operation', 'num', 'created_at', 'updated_at'], 'integer']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => '充值记录自增ID',
+            'member_id' => '会员ID',
+            'type' => '币种',
+            'operation' => '',
+            'num' => '金额',
+            'created_at' => '创建时间 奖金获得时间',
+            'updated_at' => '更新时间',
+        ];
+    }
+
+    /**
+    public function beforeSave($insert) {
+        if ($this->isNewRecord) {
+            $this->createDate = date('Y-m-d H:i:s');
+            $this->userCreate = Yii::$app->user->id;
+            $this->userUpdate = Yii::$app->user->id;
+        } else {
+            $this->updateDate = date('Y-m-d H:i:s');
+            $this->userUpdate = Yii::$app->user->id;
+        }
+        return parent::beforeSave($insert);
+    }
+    
+    public function getUserCreateLabel() {
+
+        $user = User::find()->select('username')->where(['id' => $this->userCreate])->one();
+        return $user->username;
+    }
+
+    public function getUserUpdateLabel() {
+        $user = User::find()->select('username')->where(['id' => $this->userUpdate])->one();
+        return $user->username;
+    }
+
+    */
+
+    //充值和扣除
+    public function deposit($data)
+    {
+        $id = $data['Deposit']['member_id'];
+        $result = Member::findOne(['id' => $id]);
+        if ($data['Deposit']['operation'] == 2 ) {
+            if ($result->a_coin < $data['Deposit']['num'] || $result->b_coin < $data['Deposit']['num']) {
+
+                Yii::$app->session->setFlash('error', '余额不足,扣除失败');
+                return null;
+            }
+        }
+
+        if ($this->validate()) {
+
+            if ($this->save()) {
+                return $this;
+            }
+        }
+
+        return null;
+    }
+}
