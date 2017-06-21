45250ea8a32f4041e6c1b049dbc58159dc2024ef
harlen-angke-mac <377304925@qq.com>
2017-06-21 17:47:57 +0800

    1.修正一处文字错误

--- api/controllers/MemberController.php
+++ api/controllers/MemberController.php
@@ -251,6 +251,6 @@ class MemberController extends ApiController
         if($data = $model->log(Yii::$app->request->getQueryParam('type'), ArrayHelper::getValue(Yii::$app->session->get('member'), 'member_id'))){
             return $this->jsonReturn(1, 'success', $data);
         }
-        return $this->jsonReturn(0, '赞无记录', []);
+        return $this->jsonReturn(0, '暂无记录');
     }
 }
