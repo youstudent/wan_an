299c659ce984791bff40ca2f9115b182a6b2cec7
harlen-angke-mac <377304925@qq.com>
2017-06-23 13:48:25 +0800

    1.更新缺少的文件 - 公告的配置文件

---
+++ backend/views/home/index.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\helpers\Html;
+
+$this->title = '';
+/* @var $this yii\web\View */
+/* @var $model backend\models\Order */
+
+?>
+<div class="order-view">
+
+
+<h1>你即将进入相关权利范围内界面，请认真履行公司赋予你的任务！</h1>
+
+
+
+</div>
